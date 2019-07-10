2ec9792f3d864eb309d5ba7bdfafbe875db5400a
Dominic Simpson <52511353+DominicSimpson@users.noreply.github.com>
2019-07-10 22:35:28 +0100

    Delete owndebeauvoir.js

--- scripts/owndebeauvoir.js
+++
@@ -1,8 +0,0 @@
-function view(imgsrc) {
-      viewwin = window.open(imgsrc,'viewwin', 'width=400,height=600'); 
-   }
-   
-function view2(imgsrc) {
-      viewwin = window.open(imgsrc,'viewwin', 'width=750,height=507'); 
-   }
-   
