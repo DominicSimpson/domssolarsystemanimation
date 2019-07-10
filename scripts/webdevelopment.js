368c2b29c09d26540fa4779c67585e4993024725
Dominic Simpson <52511353+DominicSimpson@users.noreply.github.com>
2019-07-10 22:35:38 +0100

    Delete webdevelopment.js

--- scripts/webdevelopment.js
+++
@@ -1,8 +0,0 @@
-function readMore() {
-	document.getElementById("readmore").onclick = function() {
-		document.getElementById("hidden-first").style.display="inline";
-	return true;
-	}
-}
-
-window.addEventListener('load',readMore, false);
